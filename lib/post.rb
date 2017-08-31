a744629bf1a5284e92fdf6a848547ec376c8566f
cpan001 <github email address>
2017-08-31 15:27:31 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Post
+  attr_reader :title
+  attr_accessor :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+
+end
+
+# binding.pry
+#
+# ""
